1c35953ecbccef52691b6f45411985d39671583a
Nik Molnar <nikmolnar@users.noreply.github.com>
2014-07-14 15:47:46 -0700

    Added class for identify request configuration

--- ncdjango/config.py
+++ ncdjango/config.py
@@ -8,18 +8,8 @@ DEFAULT_IMAGE_FORMAT = "png"
 DEFAULT_BACKGROUND_COLOR = Color(0, 0, 0, 0)
 
 
-class RenderConfiguration(object):
-    """Properties for a render image request"""
-
-    def __init__(self, variable, **kwargs):
-        self.variable = variable
-        self.extent = kwargs.get('extent', variable.service.full_extent)
-        self.renderer = kwargs.get('render', variable.renderer)
-        self.projection = kwargs.get('projection', variable.service.projection)
-        self.size = kwargs.get('size', DEFAULT_IMAGE_SIZE)
-        self.time_index = kwargs.get('time_index')
-        self.format = kwargs.get('format', DEFAULT_IMAGE_FORMAT)
-        self.background_color = kwargs.get('background_color', DEFAULT_BACKGROUND_COLOR)
+class ConfigurationBase(object):
+    """Base request configuration class"""
 
     def set_time_index_from_datetime(self, value, best_fit=True):
         """
@@ -30,18 +20,31 @@ class RenderConfiguration(object):
 
         steps = self.variable.service.time_steps
         if value in steps:
-            return steps.index(value)
+            self.time_index = steps.index(value)
         elif best_fit:
-            return utils.best_fit(steps, value)
+            self.time_index = utils.best_fit(steps, value)
         else:
             raise ValueError("Invalid date")
 
+
+class RenderConfiguration(ConfigurationBase):
+    """Properties for a render image request"""
+
+    def __init__(self, variable, **kwargs):
+        self.variable = variable
+        self.extent = kwargs.get('extent', variable.service.full_extent)
+        self.renderer = kwargs.get('render', variable.renderer)
+        self.size = kwargs.get('size', DEFAULT_IMAGE_SIZE)
+        self.time_index = kwargs.get('time_index')
+        self.format = kwargs.get('format', DEFAULT_IMAGE_FORMAT)
+        self.background_color = kwargs.get('background_color', DEFAULT_BACKGROUND_COLOR)
 
     @property
     def hash(self):
         """
         Returns a hash of this render configuration from the variable, renderer, and time_index parameters. Used for
-        caching the full-extent, native projection render so that subsequent requests can be served by a warp operation only.
+        caching the full-extent, native projection render so that subsequent requests can be served by a warp
+        operation only.
         """
 
         renderer_str = "{}|{}|{}|{}".format(
@@ -53,5 +56,16 @@ class RenderConfiguration(object):
         elif isinstance(self.renderer, UniqueValuesRenderer):
             renderer_str = "{}|{}".format(renderer_str, self.renderer.labels)
 
-
         return hash("{}/{}/{}".format(self.variable.pk, renderer_str, self.time_index))
+
+
+class IdentifyConfiguration(ConfigurationBase):
+    """Properties for an identify value request"""
+
+    ALL_TIME = -1
+
+    def __init__(self, variable, point, projection, time_index=ALL_TIME):
+        self.variable = variable
+        self.point = point
+        self.projection = projection
+        self.time_index = time_index
