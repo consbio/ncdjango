e5ebd685181a3d161723e723987f51c1ce82db88
Nik Molnar <nikmolnar@users.noreply.github.com>
2014-07-22 15:38:35 -0700

    Further arcgis interface implementation and updates

--- ncdjango/interfaces/arcgis/utils.py
+++ ncdjango/interfaces/arcgis/utils.py
@@ -8,3 +8,9 @@ def date_to_timestamp(date_obj):
 
 def timestamp_to_date(timestamp):
     return datetime.utcfromtimestamp(0).replace(tzinfo=utc) + timedelta(seconds=int(timestamp / 1000))
+
+
+def extent_to_envelope(extent, wkid):
+    extent = {k: getattr(extent, k) for k in ('xmin', 'ymin', 'xmax', 'ymax')}
+    extent['spatialReference'] = {'wkid': wkid}
+    return extent
