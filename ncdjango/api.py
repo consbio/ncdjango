950275b0d6fbbac194881a0853ca3dff9be8224e
nikmolnar <nik.molnar@consbio.org>
2014-10-17 17:44:47 -0700

    Treat proj4 variable attribute as primary source for projection and use CF conventions as fallback (instead of the other way around)

--- ncdjango/api.py
+++ ncdjango/api.py
@@ -154,10 +154,10 @@ class TemporaryFileResource(ModelResource):
                             })
                         except (OcgException, CFException):
                             pass
-                    else:
-                        variable_info = dataset.variables[variable[0]]
-                        if 'proj4' in variable_info.ncattrs():
-                            data['variables'][variable[0]]['proj4'] = variable_info.proj4
+
+                    dataset_variable = dataset.variables[variable[0]]
+                    if 'proj4' in dataset_variable.ncattrs():
+                        data['variables'][variable[0]]['proj4'] = dataset_variable.proj4
 
             bundle.data = data
             return self.create_response(request, bundle)
