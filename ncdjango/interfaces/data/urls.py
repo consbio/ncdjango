ed9cbbda0480745deee104dc9d9f6149a780dda5
Nik Molnar <nikmolnar@users.noreply.github.com>
2015-02-23 16:26:46 -0800

    Improved cache load times, fixed bug in data interface URL pattern

--- ncdjango/interfaces/data/urls.py
+++ ncdjango/interfaces/data/urls.py
@@ -3,7 +3,7 @@ from ncdjango.interfaces.data.views import RangeView, ClassifyView, UniqueValues
 
 
 urlpatterns = patterns('',
-    url(r'^data/services/(?P<service_name>[\w\-/]+)/(?P<variable_name>[\w\-/]+)/info/', include(patterns('',
+    url(r'^data/services/(?P<service_name>[\w\-\./]+)/(?P<variable_name>[\w\-/]+)/info/', include(patterns('',
         url(r'^range/$', RangeView.as_view(), name='data_range'),
         url(r'^classify/$', ClassifyView.as_view(), name='data_classify'),
         url(r'^unique/$', UniqueValuesView.as_view(), name='data_unique'),
