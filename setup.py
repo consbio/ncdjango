04a6b518c148b671dacc651f5673710dcaccde9f
Nik Molnar <nikmolnar@users.noreply.github.com>
2015-03-18 10:31:58 -0700

    Added setup.py, version number

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='ncdjango',
+    description='A map server for NetCDF data',
+    keywords='netcdf,django,map server',
+    version='0.2.0',
+    packages=[
+        'ncdjango', 'ncdjango.migrations', 'ncdjango.interfaces', 'ncdjango.interfaces.arcgis',
+        'ncdjango.interfaces.arcgis_extended', 'ncdjango.interfaces.data'
+    ],
+    install_requires=[
+        'clover', 'six', 'requests', 'Django>=1.7.0', 'Pillow>=2.7.0', 'Shapely>=1.3.2', 'GDAL>=1.11.0',
+        'django-tastypie>=0.11.1', 'netCDF4>=1.1.6', 'numpy>=1.8.1', 'pyproj>=1.9.4',
+    ],
+    url='https://bitbucket.com/databasin/ncdjango',
+    license='BSD',
+)
