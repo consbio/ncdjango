a8ca6b2e4766296a84aa9c537766224087788937
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-09-06 09:45:42 -0700

    Update setup and ignore file

--- setup.py
+++ setup.py
@@ -10,9 +10,12 @@ setup(
         'ncdjango.interfaces.arcgis', 'ncdjango.interfaces.arcgis_extended', 'ncdjango.interfaces.data'
     ],
     install_requires=[
-        'clover', 'six', 'requests', 'Django>=1.7.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'GDAL>=1.11.0',
+        'six', 'requests', 'Django>=1.7.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'GDAL>=1.11.0',
         'django-tastypie>=0.11.1', 'netCDF4>=1.1.6', 'numpy>=1.8.1', 'pyproj>=1.9.4', 'fiona', 'rasterio>=0.28.0',
-        'ply>=3.8', 'celery>=3.1.19', 'djangorestframework'
+        'clover', 'ply>=3.8', 'celery>=3.1.19', 'djangorestframework'
+    ],
+    dependency_links=[
+        'git+https://github.com/consbio/clover.git'
     ],
     url='https://github.com/consbio/ncdjango',
     license='BSD',
