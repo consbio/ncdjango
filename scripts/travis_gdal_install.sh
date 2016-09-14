d04f818da3a319c421fc2941d17600eba5e7c4a4
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-09-14 16:31:08 -0700

    Add travis config

---
+++ scripts/travis_gdal_install.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# originally contributed by @rbuffat to Toblerity/Fiona
+set -ex
+
+GDALOPTS="  --with-ogr \
+            --with-geos \
+            --with-expat \
+            --without-libtool \
+            --with-libz=internal \
+            --with-libtiff=internal \
+            --with-geotiff=internal \
+            --without-gif \
+            --without-pg \
+            --without-grass \
+            --without-libgrass \
+            --without-cfitsio \
+            --without-pcraster \
+            --without-netcdf \
+            --with-png \
+            --with-jpeg=internal \
+            --without-gif \
+            --without-ogdi \
+            --without-fme \
+            --without-hdf4 \
+            --with-hdf5 \
+            --without-jasper \
+            --without-ecw \
+            --without-kakadu \
+            --without-mrsid \
+            --without-jp2mrsid \
+            --without-bsb \
+            --without-grib \
+            --without-mysql \
+            --without-ingres \
+            --without-xerces \
+            --without-odbc \
+            --with-curl \
+            --without-sqlite3 \
+            --without-dwgdirect \
+            --without-idb \
+            --without-sde \
+            --without-perl \
+            --without-php \
+            --without-ruby \
+            --without-python \
+            --with-static-proj4=/usr/lib"
+
+# Create build dir if not exists
+if [ ! -d "$GDALBUILD" ]; then
+  mkdir $GDALBUILD;
+fi
+
+if [ ! -d "$GDALINST" ]; then
+  mkdir $GDALINST;
+fi
+
+ls -l $GDALINST
+
+if [ ! -d $GDALINST/gdal-2.0.2 ]; then
+  cd $GDALBUILD
+  wget http://download.osgeo.org/gdal/2.0.2/gdal-2.0.2.tar.gz
+  tar -xzf gdal-2.0.2.tar.gz
+  cd gdal-2.0.2
+  ./configure --prefix=$GDALINST/gdal-2.0.2 $GDALOPTS
+  make -s -j 2
+  make install
+fi
+
+# change back to travis build dir
+cd $TRAVIS_BUILD_DIR
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'ncdjango.interfaces.arcgis', 'ncdjango.interfaces.arcgis_extended', 'ncdjango.interfaces.data'
     ],
     install_requires=[
-        'six', 'requests', 'Django>=1.7.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'GDAL>=1.11.0',
+        'six', 'requests', 'Django>=1.7.0,<1.9.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'GDAL>=1.11.0',
         'django-tastypie>=0.11.1', 'netCDF4>=1.1.6', 'numpy>=1.8.1', 'pyproj>=1.9.4', 'fiona', 'rasterio>=0.28.0',
         'clover', 'ply>=3.8', 'celery>=3.1.19', 'djangorestframework'
     ],
