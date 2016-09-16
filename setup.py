c983c4dc27c547dab0c5f4afd5438231d26ce840
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-09-16 16:47:47 -0700

    Remove GDAL requirement (it's not a direct dependency)

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
         'ncdjango.interfaces.arcgis', 'ncdjango.interfaces.arcgis_extended', 'ncdjango.interfaces.data'
     ],
     install_requires=[
-        'six', 'requests', 'Django>=1.7.0,<1.9.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'GDAL>=1.11.0',
-        'django-tastypie>=0.11.1', 'netCDF4>=1.1.6', 'numpy>=1.8.1', 'pyproj>=1.9.4', 'fiona', 'rasterio>=0.28.0',
-        'clover', 'ply>=3.8', 'celery>=3.1.19', 'djangorestframework'
+        'six', 'requests', 'Django>=1.7.0,<1.9.0', 'Pillow>=2.9.0', 'Shapely>=1.3.2', 'django-tastypie>=0.11.1',
+        'netCDF4>=1.1.6', 'numpy>=1.8.1', 'pyproj>=1.9.4', 'fiona', 'rasterio>=0.28.0', 'clover', 'ply>=3.8',
+        'celery>=3.1.19', 'djangorestframework'
     ],
     dependency_links=[
         'git+https://github.com/consbio/clover.git'
